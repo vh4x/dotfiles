0c3a485fb3ecf8288f6a5d0f37ed820040657a30
Víctor <templar996@gmail.com>
2013-07-16 20:09:06 -0600

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,4 +79,9 @@
 ;; ---------------------------
 ;; -- Auto-complete for parentheses and brackets --
 ;; ---------------------------
+ (defun electric-pair ()
+      "If at end of line, insert character pair without surrounding spaces.
+    Otherwise, just insert the typed character."
+      (interactive)
+      (if (eolp) (let (parens-require-spaces) (insert-pair)) (self-insert-command 1)))
 (electric-pair-mode 1)
